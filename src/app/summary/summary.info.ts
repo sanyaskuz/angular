807a89600f2b640de6129575a0455e1904106020
sanyaskuz <tatanakovtun@MacBook-Air-Mac.local>
2022-09-13 22:16:49 +0300

    first

---
+++ src/app/summary/summary.info.ts
@@ -0,0 +1,8 @@
+export class infoSummary{
+  tel:number=0;
+  name:string=''
+  surname:string='';
+  dataBirth:any;
+  address:string='';
+  salary:number=0;
+}
---
+++ src/app/summary/workExperience.ts
@@ -0,0 +1,7 @@
+export class experience{
+  work:string='';
+  text:string='';
+  skill:string='';
+  skill1:string='';
+  skill2:string="";
+}
